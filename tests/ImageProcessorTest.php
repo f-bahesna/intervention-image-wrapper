ceb8ab4a49402abb2ca7fbcfbbad69e134146b6a
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-11-27 22:42:53 +0700

    enhance(tests): enhance testcase resize image

--- tests/ImageProcessorTest.php
+++ tests/ImageProcessorTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace Tests;
 
 use Fbahesna\InterventionImageWrapper\Facades\ImageWrapper;
+use Fbahesna\InterventionImageWrapper\Services\ImageWrapperService;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @author frada <fbahezna@gmail.com>
@@ -43,5 +46,41 @@ class ImageProcessorTest extends TestCase
         $this->assertFileExists($storagePath.'/'.$output);
 
         fwrite(STDOUT, "Output file is at: {$storagePath}/{$output}\n");
+    }
+
+    public function test_it_can_resize_image()
+    {
+        Storage::fake('local');
+
+        $image = UploadedFile::fake()->image('horse.jpg', 4000, 4000);
+
+        $originalPath = __DIR__.'/images/horse.jpg';
+        Storage::disk('local')->put($originalPath, file_get_contents($image->getRealPath()));
+
+        $originalSize = Storage::disk('local')->size($originalPath);
+
+        $optimizedPath = __DIR__.'/optimized/horse.jpg';
+
+        fwrite(STDOUT, "[TEST] Start resizing image\n");
+
+        $this->wrapperService()
+            ->load($image)
+            ->resize(700, 700)
+            ->store($optimizedPath);
+
+        $optimizedSize = Storage::disk('local')->size($optimizedPath);
+
+        $this->assertLessThan(
+            $originalSize,
+            $optimizedSize,
+            "Optimized should reduce the image size."
+        );
+
+        fwrite(STDOUT, "[TEST] Successfully resizing image from ${originalSize} bytes to ${optimizedSize} bytes\n");
+    }
+
+    private function wrapperService()
+    {
+        return $this->app->make(ImageWrapperService::class);
     }
 }
